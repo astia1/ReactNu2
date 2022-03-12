1840a34cfc3593bd4ab8b7519ae2702fe9c6fc73
“Astia1” <john@mckinneyphoto.com>
2022-03-12 13:22:18 -0800

    pre-workshop

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
 import Contact from "./Contact";
+import About from "./About";
+import CampsiteInfo from "./CampsiteInfo";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
@@ -36,6 +38,21 @@ class Main extends Component {
       );
     };
 
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -45,6 +62,12 @@ class Main extends Component {
             exact
             path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+          <Route
+            exact
+            path="/aboutus"
+            render={() => <About partners={this.state.partners} />}
           />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
